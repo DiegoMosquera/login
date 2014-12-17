41ce1adb2a9267dc313057914b184c967caa8f5d
Diego Mosquera <diegomosquera94@gmail.com>
2014-12-17 17:04:50 -0500

    Iniciando projecto

---
+++ app/database/migrations/2014_12_17_215807_crear_usuarios.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearUsuarios extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('usuarios', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string ('username',50);
+			$table->string ('email',50);
+			$table->string ('password',20);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('usuarios');
+	}
+
+}
